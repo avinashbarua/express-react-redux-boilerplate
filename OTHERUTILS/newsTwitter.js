6ef864a4b9a8e0f47c25eca577b84f46e9425737
geekodour <hrishikeshbman@gmail.com>
2016-09-17 17:50:51 +0530

    centeered

---
+++ OTHERUTILS/newsTwitter.js
@@ -0,0 +1,60 @@
+/*var MongoClient = require('mongodb').MongoClient, test = require('assert');
+// Connection url
+var url = 'mongodb://geekodour:geekz@ds029436.mlab.com:29436/newshub';
+// Connect using MongoClient
+MongoClient.connect(url, function(err, db) {
+    var poop = db.collection('poop');
+    var p = poop.find({}).toArray(function(err,items){
+        console.log(items);
+    });
+    db.close();
+});
+*/
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key:         'MwKvY5bfK7qRoHHKiFrFItS3P',
+  consumer_secret:      'V7QeqYUPOqqkdMfdUJLqOHMxQFvFqR91JJROPJq1j28O12XPVb',
+  access_token_key:     '1513607755-3MWvh70AgeOXj1LCpWifGdX254EhD4AermoW8Pt',
+  access_token_secret:  'MQyCFs9YNSHCT2V4GTGwhFi0swoC1dVqYKVFvJEkGN1cL',
+});
+ 
+
+var getHashTweets = function(){
+    return new Promise(function(resolve,reject){
+      client.get('trends/place', {id:1}, function(error, tweets, response) {
+        if (error) {
+          reject(error);
+        }
+        else {
+         var hashtags = tweets[0].trends
+                    .filter(function(hashtag){return hashtag.name[0] == "#"})
+                    .map(function(hashtag, index){
+                         return {name: hashtag.name, 
+                         tweet_volume: hashtag.tweet_volume,
+                         toggle: false,
+                         id: index,
+                         tweets: []}
+                    }); 
+          resolve(hashtags);
+        }
+      });
+    }); // end of reurn new
+};
+var poop = [];
+getHashTweets().then(function(hashtags){
+  hashtags.map(function(hashtag){
+    client.get('search/tweets', {q:hashtag.name,count:2}, function(error, tweets, response) {
+      if (!error) {
+        var tweetsOfusers = tweets.statuses
+                                      .map(function(tweet){return {id: tweet.id,
+                                                                  text: tweet.text,
+                                                                  user: tweet.user.screen_name}});
+        hashtag.tweets = tweetsOfusers;
+        console.log(hashtag);
+      }
+    });
+  });
+});
+
+
