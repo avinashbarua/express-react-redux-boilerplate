7ccb91f6cce2d4cef5282ef27e6524df907e461e
geekodour <hrishikeshbman@gmail.com>
2016-09-27 21:04:41 +0530

    all countries done

---
+++ src/components/hashtag/HashtagContainerHeader.js
@@ -0,0 +1,26 @@
+import React, {PropTypes} from 'react';
+
+const HashtagContainerHeader = () => {
+    return (
+          <div className="o-container o-container--large contentOfSkew hashtag-page-container">
+            <div className="o-grid o-grid--small-full o-grid--medium-full o-grid--large-fit o-grid--wrap">
+              <div className="o-grid__cell o-grid__cell--width-70">
+                <div className="o-grid o-grid--small-full o-grid--wrap">
+                  <div className="o-grid__cell o-grid__cell--width-100">Head</div>
+                  <div className="o-grid__cell o-grid__cell--width-100">Content</div>
+                </div>
+              </div>
+              <div className="o-grid__cell o-grid__cell--width-30">
+              second
+              </div>
+            </div>
+          </div>
+    );
+};
+
+HashtagContainerHeader.propTypes = {
+};
+
+
+
+export default HashtagContainerHeader;
